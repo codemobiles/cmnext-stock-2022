0d5621c25842afca8f283e0fb73046a315e48cc2
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-05 09:08:00 +0700

    ep10: plain formik usage

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form, Field, FormikProps } from "formik";
 
-type Props = {}
+type Props = {};
 
 export default function register({}: Props) {
   return (
-    <div>register</div>
-  )
+    <div>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(value) => alert(JSON.stringify(value))}
+      >
+        {({ handleChange, handleSubmit, values }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              id="username"
+              onChange={handleChange}
+              value={values.username}
+              type="text"
+              placeholder="username"
+            />
+            <br />
+            <input
+              id="password"
+              onChange={handleChange}
+              value={values.password}
+              type="text"
+              placeholder="password"
+            />
+            <br />
+            <button type="submit">Register </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
 }
