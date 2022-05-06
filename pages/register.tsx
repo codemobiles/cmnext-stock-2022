c2b37f95eb13b5f8c12e8463fab968e8e93d6880
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-06 09:17:21 +0700

    ep11: formik + material

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,37 +1,110 @@
-import React from "react";
+import React, { ReactElement } from "react";
+
+import { makeStyles } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { TextField } from "formik-material-ui";
 import { Formik, Form, Field, FormikProps } from "formik";
+import Router, { useRouter } from "next/router";
+import { Box } from "@mui/material";
 
 type Props = {};
 
 export default function register({}: Props) {
+  const showForm = ({
+    values,
+    setFieldValue,
+    isValid,
+    dirty,
+    handleSubmit,
+  }: FormikProps<any>) => {
+    return (
+      <Form onSubmit={handleSubmit}>
+        <Field
+          component={TextField}
+          name="username"
+          id="username"
+          margin="normal"
+          required
+          fullWidth
+          label="Username"
+          autoComplete="email"
+          autoFocus
+        />
+        <Field
+          component={TextField}
+          name="password"
+          margin="normal"
+          required
+          fullWidth
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+
+        <Button type="submit" fullWidth variant="contained" color="primary">
+          Register
+        </Button>
+        <Button
+          fullWidth
+          size="small"
+          color="primary"
+          onClick={() => Router.push("/")}
+        >
+          Cancel
+        </Button>
+      </Form>
+    );
+  };
+
   return (
-    <div>
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        onSubmit={(value) => alert(JSON.stringify(value))}
+    <React.Fragment>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
       >
-        {({ handleChange, handleSubmit, values }) => (
-          <form onSubmit={handleSubmit}>
-            <input
-              id="username"
-              onChange={handleChange}
-              value={values.username}
-              type="text"
-              placeholder="username"
-            />
-            <br />
-            <input
-              id="password"
-              onChange={handleChange}
-              value={values.password}
-              type="text"
-              placeholder="password"
-            />
-            <br />
-            <button type="submit">Register </button>
-          </form>
-        )}
-      </Formik>
-    </div>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 200 }}
+            image="/static/img/next_register.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Formik
+              initialValues={{ username: "", password: "" }}
+              onSubmit={async (values) => {
+                alert(JSON.stringify(values));
+              }}
+            >
+              {(props) => showForm(props)}
+            </Formik>
+          </CardContent>
+        </Card>
+
+        <style jsx global>
+          {`
+            body {
+              min-height: 100vh;
+              position: relative;
+              margin: 0;
+              background-size: cover;
+              background-image: url("/static/img/bg4.jpg");
+              text-align: center;
+            }
+          `}
+        </style>
+      </Box>
+    </React.Fragment>
   );
 }
