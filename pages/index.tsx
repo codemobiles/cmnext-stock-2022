abdfe0cd9d2772fc3fdc0767badc68d7147f79e7
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-07 16:33:16 +0700

    ep19: access state in userSlice

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Layout from "@/components/Layouts/Layout";
 import React from "react";
+import { useSelector } from "react-redux";
 
 type Props = {};
 
-export default function index({}: Props) {
+export default function Index({}: Props) {
+  const userSelector = useSelector((store: any) => store.user);
+
   return (
     <Layout>
-      <div>Lek</div>
+      <div>Lek {userSelector.username}</div>
     </Layout>
   );
 }
