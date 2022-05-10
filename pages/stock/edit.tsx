41ce90c1086b1a127b126ffdf34742eae446ba06
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-10 21:58:06 +0700

    ep60: getServerSideProps

--- pages/stock/edit.tsx
+++ pages/stock/edit.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Layout from "@/components/Layouts/Layout";
+import withAuth from "@/components/withAuth";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import React from "react";
 
-type Props = {}
+type Props = {
+  product?: string;
+};
 
-export default function edit({}: Props) {
+const Edit = ({ product }: Props) => {
   return (
-    <div>stock/edit</div>
-  )
-}
+    <Layout>
+      <div>Edit {product}</div>
+    </Layout>
+  );
+};
+
+export default withAuth(Edit);
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const { id }: any = context.query;
+  if (id) {
+    const product = id; //await doGetStockById(id);
+    return {
+      props: {
+        product,
+      },
+    };
+  } else {
+    return { props: {} };
+  }
+};
