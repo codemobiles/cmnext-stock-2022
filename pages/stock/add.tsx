96216a5bc4c342d0cdccc24d16c02a55c48992b0
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-11 21:48:12 +0700

    ep69: add product form

--- pages/stock/add.tsx
+++ pages/stock/add.tsx
@@ -1,7 +1,175 @@
+import Layout from "@/components/Layouts/Layout";
+import withAuth from "@/components/withAuth";
+import { ProductData } from "@/models/product.model";
+import {
+  addProduct,
+  doGetStockById,
+  editProduct,
+} from "@/services/serverService";
+import { productImageURL } from "@/utils/commonUtil";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+import { Field, Form, Formik, FormikProps } from "formik";
+import { TextField } from "formik-material-ui";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 type Props = {};
 
-export default function add({}: Props) {
-  return <div>stock/add</div>;
-}
+const Add = ({}: Props) => {
+  const router = useRouter();
+
+  const showForm = ({
+    values,
+    setFieldValue,
+    isValid,
+  }: FormikProps<ProductData>) => {
+    return (
+      <Form>
+        <Card>
+          <CardContent sx={{ padding: 4 }}>
+            <Typography gutterBottom variant="h3">
+              Add Stock
+            </Typography>
+
+            <Field
+              style={{ marginTop: 16 }}
+              fullWidth
+              component={TextField}
+              name="name"
+              type="text"
+              label="Name"
+            />
+            <br />
+            <Field
+              style={{ marginTop: 16 }}
+              fullWidth
+              component={TextField}
+              name="price"
+              type="number"
+              label="Price"
+            />
+
+            <Field
+              style={{ marginTop: 16 }}
+              fullWidth
+              component={TextField}
+              name="stock"
+              type="number"
+              label="Stock"
+            />
+
+            <div style={{ margin: 16 }}>{showPreviewImage(values)}</div>
+
+            <div>
+              <Image
+                objectFit="cover"
+                alt="product image"
+                src="/static/img/ic_photo.png"
+                width={25}
+                height={20}
+              />
+              <span style={{ color: "#00B0CD", marginLeft: 10 }}>
+                Add Picture
+              </span>
+
+              <input
+                type="file"
+                onChange={(e: React.ChangeEvent<any>) => {
+                  e.preventDefault();
+                  setFieldValue("file", e.target.files[0]); // for upload
+                  setFieldValue(
+                    "file_obj",
+                    URL.createObjectURL(e.target.files[0])
+                  ); // for preview image
+                }}
+                name="image"
+                click-type="type1"
+                multiple
+                accept="image/*"
+                id="files"
+                style={{ padding: "20px 0 0 20px" }}
+              />
+            </div>
+          </CardContent>
+          <CardActions>
+            <Button
+              disabled={!isValid}
+              fullWidth
+              variant="contained"
+              color="primary"
+              type="submit"
+              sx={{ marginRight: 1 }}
+            >
+              Add
+            </Button>
+            <Link href="/stock" passHref>
+              <Button variant="outlined" fullWidth>
+                Cancl
+              </Button>
+            </Link>
+          </CardActions>
+        </Card>
+      </Form>
+    );
+  };
+
+  const showPreviewImage = (values: any) => {
+    if (values.file_obj) {
+      return (
+        <Image
+          objectFit="contain"
+          alt="product image"
+          src={values.file_obj}
+          width={100}
+          height={100}
+        />
+      );
+    }
+  };
+
+  const initialValues: ProductData = {
+    name: "",
+    stock: 0,
+    price: 0,
+  };
+
+  return (
+    <Layout>
+      <Formik
+        validate={(values) => {
+          let errors: any = {};
+          if (!values.name) errors.name = "Enter name";
+          if (values.stock < 3) errors.stock = "Min stock is not lower than 3";
+          if (values.price < 3) errors.price = "Min price is not lower than 3";
+          return errors;
+        }}
+        initialValues={initialValues}
+        onSubmit={async (values, { setSubmitting }) => {
+          let data = new FormData();
+          data.append("name", values.name);
+          data.append("price", String(values.price));
+          data.append("stock", String(values.stock));
+          if (values.file) {
+            data.append("image", values.file);
+          }
+          await addProduct(data);
+          router.push("/stock");
+          setSubmitting(false);
+        }}
+      >
+        {(props) => showForm(props)}
+      </Formik>
+    </Layout>
+  );
+};
+
+export default withAuth(Add);
