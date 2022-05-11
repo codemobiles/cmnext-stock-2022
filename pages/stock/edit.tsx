0338e5e160b70419101374dc1ec8947dbe82ec73
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-11 09:33:07 +0700

    ep62: edit form

--- pages/stock/edit.tsx
+++ pages/stock/edit.tsx
@@ -2,17 +2,150 @@ import Layout from "@/components/Layouts/Layout";
 import withAuth from "@/components/withAuth";
 import { ProductData } from "@/models/product.model";
 import { doGetStockById } from "@/services/serverService";
+import { productImageURL } from "@/utils/commonUtil";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+import { Field, Form, Formik, FormikProps } from "formik";
+import { TextField } from "formik-material-ui";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 type Props = {
   product?: ProductData;
 };
 
 const Edit = ({ product }: Props) => {
+ 
+ 
+  const showForm = ({ values, setFieldValue }: FormikProps<ProductData>) => {
+    return (
+      <Form>
+        <Card>
+          <CardContent sx={{ padding: 4 }}>
+            <Typography gutterBottom variant="h3">
+              Edit Stock
+            </Typography>
+
+            <Field
+              style={{ marginTop: 16 }}
+              fullWidth
+              component={TextField}
+              name="name"
+              type="text"
+              label="Name"
+            />
+            <br />
+            <Field
+              style={{ marginTop: 16 }}
+              fullWidth
+              component={TextField}
+              name="price"
+              type="number"
+              label="Price"
+            />
+
+            <Field
+              style={{ marginTop: 16 }}
+              fullWidth
+              component={TextField}
+              name="stock"
+              type="number"
+              label="Stock"
+            />
+
+            <div style={{ margin: 16 }}>{showPreviewImage(values)}</div>
+
+            <div>
+              <Image
+                objectFit="cover"
+                alt="product image"
+                src="/static/img/ic_photo.png"
+                width={25}
+                height={20}
+              />
+              <span style={{ color: "#00B0CD", marginLeft: 10 }}>
+                Add Picture
+              </span>
+
+              <input
+                type="file"
+                onChange={(e: React.ChangeEvent<any>) => {
+                  e.preventDefault();
+                  setFieldValue("file", e.target.files[0]); // for upload
+                  setFieldValue(
+                    "file_obj",
+                    URL.createObjectURL(e.target.files[0])
+                  ); // for preview image
+                }}
+                name="image"
+                click-type="type1"
+                multiple
+                accept="image/*"
+                id="files"
+                style={{ padding: "20px 0 0 20px" }}
+              />
+            </div>
+          </CardContent>
+          <CardActions>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              type="submit"
+              sx={{ marginRight: 1 }}
+            >
+              Edit
+            </Button>
+            <Link href="/stock" passHref>
+              <Button variant="outlined" fullWidth>
+                Cancl
+              </Button>
+            </Link>
+          </CardActions>
+        </Card>
+      </Form>
+    );
+  };
+
+  const showPreviewImage = (values: any) => {
+    if (values.file_obj) {
+      return (
+        <Image
+          objectFit="contain"
+          alt="product image"
+          src={values.file_obj}
+          width={100}
+          height={100}
+        />
+      );
+    } else if (values.image) {
+      return (
+        <Image
+          objectFit="contain"
+          alt="product image"
+          src={productImageURL(values.image)}
+          width={100}
+          height={100}
+        />
+      );
+    }
+  };
+
   return (
     <Layout>
-      <div>Edit {JSON.stringify(product)}</div>
+      <Formik
+        initialValues={product!}
+        onSubmit={(values, { setSubmitting }) => {}}
+      >
+        {(props) => showForm(props)}
+      </Formik>
     </Layout>
   );
 };
