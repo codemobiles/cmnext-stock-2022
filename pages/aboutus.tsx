19fe15cdb1254f829c6be1b4ed9703d134b3ae82
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-12 10:55:35 +0700

    ep74: about us

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -1,13 +1,24 @@
 import Layout from "@/components/Layouts/Layout";
 import withAuth from "@/components/withAuth";
+import { Paper } from "@mui/material";
 import React from "react";
+import Iframe from "react-iframe";
 
 type Props = {};
 
 const AboutUs = ({}: Props) => {
   return (
     <Layout>
-      <div>AboutUs</div>
+      <Paper sx={{ height: "86vh" }}>
+        <Iframe
+          url="https://codemobiles.com"
+          width="100%"
+          height="100%"
+          id="myId"
+          display="inline"
+          position="relative"
+        />
+      </Paper>
     </Layout>
   );
 };
