4ac91a1c42e6235091b51fe15bc1d170bfb8b762
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-12 10:00:58 +0700

    ep71: finish setup chartjs

--- pages/report.tsx
+++ pages/report.tsx
@@ -3,7 +3,6 @@ import withAuth from "@/components/withAuth";
 import React from "react";
 import "chart.js/auto";
 import { Chart, Line } from "react-chartjs-2";
-import faker from "faker";
 
 type Props = {};
 
@@ -22,18 +21,26 @@ export const options = {
 
 const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
+function getRandomInt(): any {
+  let randoms = [];
+  for (let index = 0; index <= labels.length; index++) {
+    randoms.push(Math.floor(Math.random() * (50000 - 5 + 1)) + 5);
+  }
+  return randoms;
+}
+
 export const data = {
   labels,
   datasets: [
     {
       label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => getRandomInt()),
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
       label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => getRandomInt()),
       borderColor: "rgb(53, 162, 235)",
       backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
